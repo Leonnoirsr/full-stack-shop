a67fb9eae9f2a4b29a7d14d0e567e924a1d87bc7
Leon Huggins <lhuggins@corval.io>
2023-09-27 12:34:36 -0400

    Added: MVC Design Pattern, implemented first model (Product), added admin control functions in Controllers folder.

---
+++ models/product.js
@@ -0,0 +1,46 @@
+const fs   = require( 'fs' )
+const path = require( 'path' )
+
+const products = []
+
+module.exports = class Product {
+	
+	constructor( title ){
+		this.title = title;
+	}
+	
+	save(){
+		const p = path.join( path.dirname( process.mainModule.filename ),
+			'data',
+			'products.json'
+		);
+		fs.readFile(p, (err, fileContent) => {
+			
+			let products = [];
+			
+			if(!err && fileContent.toString().trim()) {
+				products = JSON.parse(fileContent.toString());
+			}
+			products.push(this)
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err)
+			})
+		});
+	}
+	
+	static fetchAll(){
+		
+		const p = path.join( path.dirname( process.mainModule.filename ),
+			'data',
+			'products.json'
+		);
+		
+		fs.readFile(p, (err, fileContent) => {
+			if(err) {
+				return [];
+			}
+			return JSON.parse(fileContent.toString());
+		})
+	}
+	
+}
