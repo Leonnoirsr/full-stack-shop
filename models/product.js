1e1ae5b19700fd0fc36c4f9848ce70255878409e
Leon Huggins <lhuggins@corval.io>
2023-10-07 16:01:59 -0400

    Added: Dynamic routes and parameters. Update: model to add new function, controller to add new function.

--- models/product.js
+++ models/product.js
@@ -1,46 +1,51 @@
-const fs   = require( 'fs' )
-const path = require( 'path' )
+const fs   = require( 'fs' );
+const path = require( 'path' );
 
-const products = []
+const p = path.join(
+	path.dirname( process.mainModule.filename ),
+	'data',
+	'products.json'
+);
+
+const getProductsFromFile = cb => {
+	fs.readFile( p, ( err, fileContent ) => {
+		if( err ){
+			cb( [] );
+		} else {
+			const products = JSON.parse(fileContent);
+//			console.log(products); // Add this line to log the products array
+			cb(products);
+		}
+	} );
+};
 
 module.exports = class Product {
-	
-	constructor( title ){
-		this.title = title;
+	constructor( title, imageUrl, description, price ){
+		this.title       = title;
+		this.imageUrl    = imageUrl;
+		this.description = description;
+		this.price       = price;
 	}
 	
 	save(){
-		const p = path.join( path.dirname( process.mainModule.filename ),
-			'data',
-			'products.json'
-		);
-		fs.readFile(p, (err, fileContent) => {
-			
-			let products = [];
-			
-			if(!err && fileContent.toString().trim()) {
-				products = JSON.parse(fileContent.toString());
-			}
-			products.push(this)
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				console.log(err)
-			})
-		});
+		this.id = Math.random().toString();
+		getProductsFromFile( products => {
+			products.push( this );
+			fs.writeFile( p, JSON.stringify( products ), err => {
+				console.log( err );
+			} );
+		} );
 	}
 	
-	static fetchAll(){
-		
-		const p = path.join( path.dirname( process.mainModule.filename ),
-			'data',
-			'products.json'
-		);
-		
-		fs.readFile(p, (err, fileContent) => {
-			if(err) {
-				return [];
-			}
-			return JSON.parse(fileContent.toString());
-		})
+	static fetchAll( cb ){
+		getProductsFromFile( cb );
 	}
 	
-}
+	static findById( id, cb ){
+		getProductsFromFile( products => {
+			const product = products.find( p => p.id.toString() === id.toString() );
+			cb( product );
+		} )
+	}
+	
+};
--- util/path.js
+++ util/path.js
@@ -1,3 +1,3 @@
-const path = require('path');
+const path = require('full-stack-shop/util/path');
 
 module.exports = path.dirname(process.mainModule.filename);
