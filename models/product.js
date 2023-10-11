32ade2d96ada3ae17205625510ad7041e7f458eb
Leon Huggins <lhuggins@corval.io>
2023-10-11 17:21:43 -0400

    Added: Edit product logic, updated product model for editing logic.

--- models/product.js
+++ models/product.js
@@ -12,29 +12,43 @@ const getProductsFromFile = cb => {
 		if( err ){
 			cb( [] );
 		} else {
-			const products = JSON.parse(fileContent);
-//			console.log(products); // Add this line to log the products array
-			cb(products);
+			const products = JSON.parse( fileContent );
+			//			console.log(products); // Add this line to log the products array
+			cb( products );
 		}
 	} );
 };
 
 module.exports = class Product {
-	constructor( title, imageUrl, description, price ){
+	constructor( id, title, imageUrl, description, price ){
+		this.id          = id;
 		this.title       = title;
 		this.imageUrl    = imageUrl;
 		this.description = description;
 		this.price       = price;
 	}
 	
-	save(){
-		this.id = Math.random().toString();
-		getProductsFromFile( products => {
-			products.push( this );
-			fs.writeFile( p, JSON.stringify( products ), err => {
-				console.log( err );
-			} );
-		} );
+	
+	save() {
+		getProductsFromFile(products => {
+			if (this.id) {
+				const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+				
+				if (existingProductIndex !== -1) {
+					products[existingProductIndex] = this;
+				} else {
+					console.log("Product with the specified ID does not exist.");
+					return;
+				}
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+			}
+			
+			fs.writeFile(p, JSON.stringify(products), err => {
+				console.log(err);
+			});
+		});
 	}
 	
 	static fetchAll( cb ){
