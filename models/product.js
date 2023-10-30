2a524becfe3b63a9f45d8fd42cf39330c2202017
Leon Huggins <lhuggins@corval.io>
2023-10-30 12:13:18 -0400

    Update: Removed some navigation items for the time being to work on Login functionality.

--- models/product.js
+++ models/product.js
@@ -1,35 +1,32 @@
-const getDb = require( '../util/database' ).getDB;
+const mongoose = require( 'mongoose' );
 
-class Product {
-	constructor( title, price, imageUrl, description ){
-		this.title       = title;
-		this.imageUrl    = imageUrl;
-		this.description = description;
-		this.price       = price;
-	}
-	
-	save(){
-		const db = getDb();
-		return db.collection( 'products' )
-		         .insertOne( this )
-		         .then( result => {
-			         console.log( result )
-		         } )
-		         .catch( err => console.log( err ) )
-	}
-	
-	static fetchAll(){
-		const db = getDb()
-		return db.collection( 'products' )
-		         .find()
-		         .toArray()
-		         .then(products => {
-					 console.log(products)
-			         return products;
-		         })
-		         .catch(err => console.log(err))
-	}
-	
-}
+const Schema = mongoose.Schema;
 
-module.exports = Product;
+const productSchema = new Schema( {
+	title:       {
+		type:     String,
+		required: true
+	},
+	imageUrl:    {
+		type:     String,
+		required: true
+	},
+	description: {
+		type:     String,
+		required: true
+	},
+	price:       {
+		type:     Number,
+		required: true
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	}
+	
+} )
+
+
+
+module.exports = mongoose.model('Product', productSchema)
